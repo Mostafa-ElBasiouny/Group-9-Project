60e685a658c4852b117b38fc07822663d7daf612
Mostafa Elbasiouny <Melbasiouny4793@conestogac.on.ca>
2021-12-10 10:29:44 -0500

    Add and setup unit test project.

---
+++ Group-9-Project-Unit-Tests/Group-9-Project-Unit-Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Group9ProjectUnitTests
+{
+	TEST_CLASS(Group9ProjectUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
